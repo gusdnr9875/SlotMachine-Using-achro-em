ee74e9e38df4a8a8efa0e3f57d3c61bcc46580f4
gusdnr9875 <38147253+gusdnr9875@users.noreply.github.com>
2019-12-12 16:44:56 +0900

    수정전 코드완성본

--- emded/fpga_slot.c
+++ emded/fpga_slot.c
@@ -28,6 +28,21 @@ int motor_direction;
 int motor_speed;
 unsigned char motor_state[3]; // 모터상태제어 
 //dot 동작을 위한 배열 
+//부저 동작을 위한 변수
+unsigned char state = 0; // 부저상태제어
+unsigned char retval;  // 부저
+unsigned char data1 = 1; //부저 
+int buzzer_state = 0;
+// 응용동작을 위한 함수
+unsigned char current_data[8] = { 1,2,4,8,16,32,64,128 };
+unsigned char start_data[4] = { 17,34,68,136 };
+unsigned char load_data[4] = { 24,36,66,129 };
+unsigned char exit_data[4] = { 204, 102, 51, 153 };
+//int led_state = 0; //led제어 
+//
+//int buzzer_state = 0;
+
+
 unsigned char fpga_number[10][10] = {
 	{ 0x3e,0x7f,0x63,0x73,0x73,0x6f,0x67,0x63,0x7f,0x3e }, // 0
 	{ 0x0c,0x1c,0x1c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x1e }, // 1
@@ -41,7 +56,8 @@ unsigned char fpga_number[10][10] = {
 	{ 0x3e,0x7f,0x63,0x63,0x7f,0x3f,0x03,0x03,0x03,0x03 } // 9
 };
 
-//
+
+
 void user_signal1(int sig)
 {
 	quit = 1;
@@ -60,19 +76,227 @@ void *t_function(void *data)
 
 	
 
-	while (1) {
+	for (i = 0; i < 100;i++) {
 		motor_state[0] = (unsigned char)1;
-		motor_state[1] = (unsigned char)0;
+		motor_state[1] = (unsigned char)i%2;
 		motor_state[2] = (unsigned char)255;
 		write(motor_dev, motor_state, 3);
-		usleep(1000000);
+		usleep(200000);
 	}
-	close(motor_dev);
+	//close(motor_dev);
+	//return;
+}
+
+void *t_function2(void *data)
+{
+	pid_t pid;            // process id
+	pthread_t tid;        // thread id
+
+	pid = getpid();
+	tid = pthread_self();
+	unsigned char init_led = 0;
+
+		for (int i = 0; i < 4; i++) {
+
+			write(led_dev, &start_data[i], 1);
+
+			usleep(100000);
+
+		}
+		for (int j = 3; j >= 0; j--) {
+
+			write(led_dev, &start_data[j], 1);
+
+			usleep(100000);
+
+		}
+		for (int i = 0; i < 4; i++) {
+
+			write(led_dev, &start_data[i], 1);
+
+			usleep(100000);
+
+		}
+		for (int j = 3; j >= 0; j--) {
+
+			write(led_dev, &start_data[j], 1);
+
+			usleep(100000);
+
+		}
+
+
+		for (int i = 0; i < 4; i++) {
+
+			write(led_dev, &start_data[i], 1);
+
+			usleep(100000);
+
+		}
+		for (int j = 3; j >= 0; j--) {
+
+			write(led_dev, &start_data[j], 1);
+
+			usleep(100000);
+
+		}
+		for (int i = 0; i < 4; i++) {
+
+			write(led_dev, &start_data[i], 1);
+
+			usleep(100000);
+
+		}
+		for (int j = 3; j >= 0; j--) {
+
+			write(led_dev, &start_data[j], 1);
+
+			usleep(100000);
+
+		}
+		write(led_dev , &init_led, 1);
 
 }
 
-int main(void) {
+void *t_function3(void *data) {
 
+	pid_t pid;            // process id
+	pthread_t tid;        // thread id
+
+	pid = getpid();
+	tid = pthread_self();
+	int i;
+	for (i = 0; i < 10;i++) {
+		for (int i = 0; i < 4; i++) {
+
+			write(led_dev, &start_data[i], 1);
+
+			usleep(100000);
+
+		}
+		for (int j = 3; j >= 0; j--) {
+
+			write(led_dev, &start_data[j], 1);
+
+			usleep(100000);
+
+		}
+		for (int i = 0; i < 4; i++) {
+
+			write(led_dev, &load_data[i], 1);
+
+			usleep(100000);
+
+		}
+
+		for (int j = 3; j >= 0; j--) {
+
+			write(led_dev, &load_data[j], 1);
+
+			usleep(100000);
+
+		}
+		for (int i = 0; i < 4; i++) {
+
+			write(led_dev, &exit_data[i], 1);
+
+			usleep(100000);
+
+		}
+
+		for (int j = 4; j < 0; j--) {
+
+			write(led_dev, &exit_data[j], 1);
+
+			usleep(100000);
+
+		}
+	}
+	write(led_dev, 0, 1);
+}
+
+
+
+void *t_function4(void *data) {
+
+	pid_t pid;            // process id
+	pthread_t tid;        // thread id
+
+	pid = getpid();
+	tid = pthread_self();
+
+	int i;
+	if (buzzer_state == 1) {
+		if (buzzer_state == 0) {
+			for (i = 0; i <100; i++)
+			{
+				if (i % 2 == 1)
+				{
+					data1 = 1;
+				}
+				else
+				{
+					data1 = 0;
+				}
+				retval = write(buzzer_dev, &data1, 1);
+				usleep(100000);
+			}
+		}
+		else {
+			for (int i = 0; i < 100; i++)
+			{
+				if (i % 2 == 1)
+				{
+					data1 = 1;
+				}
+				else
+				{
+					data1 = 0;
+				}
+				retval = write(buzzer_dev, &data1, 1);
+				usleep(500000);
+			}
+
+		}
+	}
+
+	else {
+		if (buzzer_state == 0) {
+			for (i = 0; i <8; i++)
+			{
+				if (i % 2 == 1)
+				{
+					data1 = 1;
+				}
+				else
+				{
+					data1 = 0;
+				}
+				retval = write(buzzer_dev, &data1, 1);
+				usleep(100000);
+			}
+		}
+		else {
+			for (int i = 0; i < 8; i++)
+			{
+				if (i % 2 == 1)
+				{
+					data1 = 1;
+				}
+				else
+				{
+					data1 = 0;
+				}
+				retval = write(buzzer_dev, &data1, 1);
+				usleep(500000);
+			}
+
+		}
+	}
+
+}
+int main(void) {
+	srand(time(NULL));
 	unsigned char push_sw_buff[9]; //푸쉬버튼버퍼
 	int buff_size; //푸쉬버튼 버퍼사이즈
 	buff_size = sizeof(push_sw_buff);
@@ -80,24 +304,28 @@ int main(void) {
 	// 기기제어를 위한 변수
 	unsigned char string[32] = "Welcome to slot machine!push btn";
 	unsigned char retval2; // fnd 를 제어하기위한 문자변수
-	char verify[4] = { '1','1','1','1' }; //fnd를 사용하기위한 코드
+	unsigned char verify[4]; //fnd를 사용하기위한 코드
+	//unsigned char data[4]; // fnd  제어를 위한 
 	
-	unsigned char state = 0; // 부저상태제어
-	unsigned char retval;  // 부저
-	unsigned char data; //부저 
 	int str_size;
 	//** 응용 동작을 위한 변수 **//
-	unsigned char data_led[4] = { 0,255,124,64 }; //led  data
+	
 	int random_num; // 도트를 위한 난수 
+	int random_num2; // 도트를 위한 난수 
+
+	
 	/*----------------*/ // TEXT_LCD
 	int button_clicked = 0; //버튼이 클릭되었는지 확인 
 
 	/*----------------*/ // 쓰레드를 위한 코드 
-	pthread_t p_thread[2];
+	pthread_t p_thread[4];
 	int thr_id;
 	int status;
 	char p1[] = "thread_1";
-
+	char p2[] = "thread_2";
+	char p3[] = "thread_3";
+	char p4[] = "thread_4";
+	int trash;
 
 	
 
@@ -143,87 +371,174 @@ int main(void) {
 		return -1;
 	}
 	
-	// init 해주는 과정 
+	while (1) {
+		// init 해주는 과정 
+		char* text_value = "0000";
+		retval2 = write(fnd_dev, text_value, 4);
 
-	(void)signal(SIGINT, user_signal1); //초기화 
+		//부저 초기화
+		data1 = 1;
+		(void)signal(SIGINT, user_signal1); // 초기화 
+		retval = write(buzzer_dev, &data1, 1);
+		//모터 초기화
+		motor_state[0] = (unsigned char)0;
+		motor_state[1] = (unsigned char)0;
+		motor_state[2] = (unsigned char)255;
+		write(motor_dev, motor_state, 3);
+		//환영메시지 출력 
+		write(text_lcd_dev, "                                ", MAX_BUFF);
 
-	//환영메시지 출력 
-	write(text_lcd_dev, string, MAX_BUFF);
-	//현란한 LED 출력
-	for (int i = 0; i < 12; i++) {
-		write(led_dev, &data_led[i%4], 1);
-		usleep(1000000);
-	}
-
-	while(1) { // 계속돌아야되서 무한루프
-		int i;
-		button_clicked = 0;
-		usleep(400000);
-		read(push_dev, &push_sw_buff, buff_size);
+		write(text_lcd_dev, "Welcome to slot machine!push btn", MAX_BUFF);
 		
-
-		for (i = 0; i<9; i++) {
-			printf("[%d] ", push_sw_buff[i]);
-			if (push_sw_buff[i] == 1) {
-				button_clicked = 1;
-				break;
-			}
+		//현란한 LED 출력
+	
+		thr_id = pthread_create(&p_thread[1], NULL, t_function2, (void *)p2);
+		// fnd 출력
+		//char* text_value = "0000";
+		for (int i = 0; i < str_size; i++)
+		{
+			verify[i] = text_value[i] - 0x30;
 		}
-		
-		if (button_clicked == 1)
-			break;
-		printf("\n");
-	}
-	// 버튼을 누른것을 인지
-	// fnd와 dot값이 움직이는 상황
-	
-	for(int i=0;i<41;i++){
-		random_num = rand() % 10;
-		str_size = sizeof(fpga_number[random_num]);
+		//retval2 = write(fnd_dev, &verify, 4);
+
+		while (1) { // 계속돌아야되서 무한루프
+			int i;
+			button_clicked = 0;
+			usleep(400000);
+			read(push_dev, &push_sw_buff, buff_size);
+
+
+			for (i = 0; i < 9; i++) {
+			//	printf("[%d] ", push_sw_buff[i]);
+				if (push_sw_buff[i] == 1) {
+					button_clicked = 1;
+					break;
+				}
+			}
+
+			if (button_clicked == 1)
+				break;
+			printf("\n");
+		}
+		// 버튼을 누른것을 인지
+		// fnd와 dot값이 움직이는 상황
+		//버튼을 누름과 동시에  모터 돌린다. 
+		motor_state[0] = (unsigned char)1;
+		motor_state[1] = (unsigned char)0;
+		motor_state[2] = (unsigned char)255;
+		write(motor_dev, motor_state, 3);
+
+		//thr_id = pthread_create(&p_thread[1], NULL, t_function2, (void *)p2);
+		//retval2 = read(fnd_dev, &verify, 4);
+
+
+		for (int i = 0; i < 41; i++) {
+			random_num = rand() % 10;
+			str_size = sizeof(fpga_number[random_num]);
+			write(dot_dev, fpga_number[random_num], str_size);
+			usleep(50000); //0.05초 
+		}
+		text_value = "0007";
+		for (int i = 0; i < str_size; i++)
+		{
+			verify[i] = text_value[i] - 0x30;
+		}
+		write(dot_dev, fpga_number[7], str_size);
+		retval2 = write(fnd_dev, &verify, 4);
+		usleep(1000000); //1초 
+
+		for (int i = 0; i < 41; i++) {
+			random_num = rand() % 10;
+			str_size = sizeof(fpga_number[random_num]);
+			write(dot_dev, fpga_number[random_num], str_size);
+			usleep(50000); //0.05초 
+		}
+		text_value = "0077";
+		for (int i = 0; i < str_size; i++)
+		{
+			verify[i] = text_value[i] - 0x30;
+		}
+		write(dot_dev, fpga_number[7], str_size);
+		retval2 = write(fnd_dev, &verify, 4);
+		usleep(1000000); //1초 
+
+		for (int i = 0; i < 41; i++) {
+			random_num = rand() % 10;
+			str_size = sizeof(fpga_number[random_num]);
+			write(dot_dev, fpga_number[random_num], str_size);
+			usleep(50000); //0.05초 
+		}
+		text_value = "0777";
+		for (int i = 0; i < str_size; i++)
+		{
+			verify[i] = text_value[i] - 0x30;
+		}
+		write(dot_dev, fpga_number[7], str_size);
+		retval2 = write(fnd_dev, &verify, 4);
+		usleep(1000000); //1초 
+
+
+		random_num2 = rand() % 20 + 41;
+
+		for (int i = 0; i < random_num2; i++) {
+			random_num = rand() % 10;
+			str_size = sizeof(fpga_number[random_num]);
+			write(dot_dev, fpga_number[random_num], str_size);
+			usleep(50000); //0.05초 
+		}
+		random_num = rand() % 5 +3;
 		write(dot_dev, fpga_number[random_num], str_size);
-		usleep(50000); //0.05초 
+		text_value = "0777";
+		char adsa[4] = { '0' + random_num,'7','7','7' };
+		text_value = adsa;
+		for (int i = 0; i < str_size; i++)
+		{
+			verify[i] = text_value[i] - 0x30;
+		}
+		//write(dot_dev, fpga_number[7], str_size);
+		retval2 = write(fnd_dev, &verify, 4);
+		usleep(1000000); //1초 
+		if (adsa[0] == '7') {
+			//성공시 
+			// 모터가 움직이는 상황
+			//쓰레드 호출 
+			thr_id = pthread_create(&p_thread[0], NULL, t_function, (void *)p1);
+			thr_id = pthread_create(&p_thread[2], NULL, t_function3, (void *)p3);
+			if (thr_id < 0)
+			{
+				perror("thread create error : ");
+				exit(0);
+			}
+
+			//write(dot_dev, 1, 10);
+
+			sleep(1);
+
+
+			write(text_lcd_dev, "                                ", MAX_BUFF);
+			//usleep(1000000); //1초 
+			//환영메시지 출력 
+			write(text_lcd_dev, "congraturation!!                ", MAX_BUFF);
+			buzzer_state = 1;
+			thr_id = pthread_create(&p_thread[3], NULL, t_function4, (void *)p4);
+			usleep(100000000); //100초 
+		}
+		else {
+			//환영메시지 출력 
+			buzzer_state = 0;
+			thr_id = pthread_create(&p_thread[3], NULL, t_function4, (void *)p4);
+			write(text_lcd_dev, "                                ", MAX_BUFF);
+			//usleep(1000000); //1초 
+			write(text_lcd_dev, "you lose!       Try again       ", MAX_BUFF);
+		}
+
+		pthread_join(p_thread[0], (void **)&trash);
+		pthread_join(p_thread[1], (void **)&trash);
+		pthread_join(p_thread[2], (void **)&trash);
+
+
+		usleep(4000000); //1초 
 	}
-
-	retval2 = read(fnd_dev, &data, 4);
-	if (retval2<0) {
-		printf("WriteError!\n");
-		return -1;
-	}
-	//성공시 
-	// 모터가 움직이는 상황
-	//쓰레드 호출 
-	thr_id = pthread_create(&p_thread[0], NULL, t_function, (void *)p1);
-
-	if (thr_id < 0)
-	{
-		perror("thread create error : ");
-		exit(0);
-	}
-	
-	// 
-
-	//write(dot_dev, 1, 10);
-
-
-
-
-	sleep(1);
-
-	
-
-	//환영메시지 출력 
-	write(text_lcd_dev, "congraturation!!", MAX_BUFF);
-
-
-	// led 출력 함수 
-	for (int i = 0; i < 120; i++) {
-		write(led_dev, &data_led[i % 4], 1);
-		usleep(1000000);
-	}
-
-	
-
-
 	close(push_dev);
 	close(dip_dev);
 	close(buzzer_dev);
@@ -233,6 +548,5 @@ int main(void) {
 	return 0;
 
 }
-
 
 
